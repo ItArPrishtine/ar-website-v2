b0345ec12b744d2db8e83e8e23eece88f7b0da4f
ItArPrishtine <73855346+ItArPrishtine@users.noreply.github.com>
2026-02-19 11:27:29 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-        client: 'postgres',
-        connection: {
-            connectionString: env('DATABASE_URL')
-        },
-        debug: true,
-        pool: { min: 0, max: 7 },
-    }
+      connectionString: env('DATABASE_URL'),
+    },
+    pool: {
+      min: 0,
+      max: 3,
+      acquireTimeoutMillis: 60000,
+      idleTimeoutMillis: 30000,
+    },
+    debug: false,
+  },
 });
